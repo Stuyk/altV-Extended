bc5d93d3bb600134462d44b41170e2cdfe44c0d9
Trevor Wessel <stuykgaming@gmail.com>
2019-06-17 10:54:08 -0600

    Update server.mjs

--- server-extended/server.mjs
+++ server-extended/server.mjs
@@ -1,106 +1,182 @@
 import * as alt from 'alt';
+import { Player, Vehicle } from 'alt';
+import * as color from './colors.mjs';
 
-export function randomColor() {
-    return Math.floor(Math.random() * 255) + 1;
-}
+console.log(`${color.FgYellow}Extended v0.1 is running.`);
 
-export function getPlayersInRange(pos, range) {
-    return alt.players.find(x => distance(x.pos, pos) <= range );
-}
-
-export function randomPositionNear(x, y, z, range, randomizeZAxis) {
-    var position = {};
-    position.x = (x + (Math.random() * (range * 2)) - range);
-    position.y = (y + (Math.random() * (range * 2)) - range);
-    if (randomizeZAxis)
-        position.z = (z + (Math.random() * (range * 2)) - range);
-    else
-        position.z = z;
-    return position;
-}
-
-export async function positionInFrontOfPlayer(player, distance) {
-    var result = await GetForwardVector(player);
-    var pos = {
-        x: player.pos.x + result.x * distance,
-        y: player.pos.y + result.y * distance,
-        z: player.pos.z + result.z * distance
-    }
-    return pos;
-}
-
-export async function getForwardVector(player) {
-    var result = await ClientCallback(player, 'getForwardVector', []);
-    return result;
-}
-
-export async function getGroundZFrom3DCoord(player, pos) {
-    var result = await ClientCallback(player, 'getGroundZFrom3DCoord', pos);
-    return result;
-}
-
-export async function ClientCallback(player, clientEventName, argsArray) {
-    alt.emitClient(player, clientEventName, argsArray);
-    
-    let promise = new Promise((res, rej) => {
-        alt.onClient(clientEventName, (player, resultsArray) => {
-            res(resultsArray);
+// This needs to be called for each player that joins the server.
+export function SetupExportsForPlayer(player) {
+    // Returns the ForwardVector of the player.
+    // player.forwardVector((res) => { console.log(res) });
+    player.forwardVector = (callback) => {
+        SetupCallback(player, 'getForwardVector', undefined, (result) => {
+            callback(result);
         });
+    }
+
+    // Returns if the player is near a position.
+    player.isNearPos = (pos, range) => {
+        var currentDistance = Distance(player.pos, pos);
+        if (currentDistance <= range)
+            return true;
+        return false;
+    }
+
+    // Create a local blip for a player.
+    player.createLocalBlip = (pos, sprite, color, scale, name, shortRange, uniqueID) => {
+        alt.emitClient(player, 'createLocalBlip', pos, sprite, color, scale, name, shortRange, uniqueID);
+    }
+
+    // Delete a local blip by uniqueID.
+    player.deleteLocalBlip = (uniqueID) => {
+        alt.emitClient(player, "deleteLocalBlip", uniqueID);
+    }
+
+    // Create a marker for a player.
+    player.createLocalMarker = (type, pos, dir, rot, scale, color, enterColor, deleteOnEnter, range, uniqueID) => {
+        alt.emitClient(player, 'createLocalMarker', type, pos, dir, rot, scale, color, enterColor, deleteOnEnter, range, uniqueID);
+    }
+
+    // Delete a marker for a player.
+    player.deleteLocalMarker = (uniqueID) => {
+        alt.emitClient(player, 'deleteLocalMarker', uniqueID);
+    }
+
+    // Show Notification to Player
+    player.showNotification = (imageName, headerMsg, detailsMsg, message) => {
+        alt.emitClient(player, 'showNotification', imageName, headerMsg, detailsMsg, message);
+    }
+
+    // Freeze Player
+    player.freeze = (state) => {
+        alt.emitClient(player, 'freezePlayer', state);
+    }
+
+    // Fade screen to black and back.
+    player.fadeScreen = (state, timeInMS) => {
+        alt.emitClient(player, 'fadeOutScreen', state, timeInMS);
+    }
+
+    // Blur screen.
+    player.blurScreen = (state, timeInMS) => {
+        alt.emitClient(player, 'blurOutScreen', state, timeInMS);
+    }
+
+    // Show Cursor
+    player.showCursor = (state) => {
+        alt.emitClient(player, 'showCursor', state);
+    }
+
+    // Show Help Text
+    player.showHelpText = (text, timeInMS) => {
+        alt.emitClient(player, 'displayHelpText', text, timeInMS)
+    }
+
+    // Show Subtitle Text
+    player.showSubtitle = (text, timeInMS) => {
+        alt.emitClient(player, 'displaySubtitle', text, timeInMS);
+    }
+
+    // Display Loading
+    player.showLoading = (text, time, type, toggled) => {
+        alt.emitClient(player, 'showLoading', text, time, type, toggled);
+    }
+}
+
+// Returns the GroundLevel from a 3D Position near a player.
+// Returns undefined if ground was not present. Otherwise a number.
+export function GetGroundZFrom3DCoord(player, pos, callback) {
+    SetupCallback(player, 'getGroundZFrom3DCoord', pos, (result) => {
+        if (result[0] == false)
+            callback(undefined);
+
+        callback(result[1]);
     });
+}
+
+// Add one vector to another.
+export function AddVector3(vector1, vector2) {
+    return {
+        x: vector1.x + vector2.x,
+        y: vector1.y + vector2.y,
+        z: vector1.z + vector2.z
+    }
+}
+
+// Subtract one vector from another.
+export function SubVector3(vector1, vector2) {
+    return {
+        x: vector1.x - vector2.x,
+        y: vector1.y - vector2.y,
+        z: vector1.z - vector2.z
+    }
+}
+
+// Returns as {r,g,b}
+export function GetRandomColor() {
+    return {
+        r: Math.floor(Math.random() * 255) + 1,
+        g: Math.floor(Math.random() * 255) + 1,
+        b: Math.floor(Math.random() * 255) + 1
+    }
+}
+
+// Get all of the players in range of a position.
+export function GetPlayersInRange(pos, range) {
+    var inRange = [];
     
-    var result = await promise;
-    return result;
+    Player.all.forEach((value) => {
+        if (Distance(pos, value.pos) > range)
+            return;
+        inRange.push(value);
+    });
+
+    return inRange;
 }
 
-
-export function isPlayerNearPosition(player, pos, distance) {
-    var currentDistance = distance(player.pos, pos);
-    if (currentDistance <= distance)
-        return true;
-    return false;
+// Get the distance between two vectors.
+export function Distance(vector1, vector2) {
+    return Math.pow(vector1.x - vector2.x, 2) + Math.pow(vector1.y - vector2.y, 2) + Math.pow(vector1.z - vector2.z, 2);
 }
 
-
-export function createLocalBlip(player, pos, sprite, color, scale, name, shortRange, uniqueID) {
-    alt.emitClient(player, 'createLocalBlip', pos, sprite, color, scale, name, shortRange, uniqueID);
+// Get Random Position Around Position
+export function RandomPosAround(pos, range) {
+    return {
+        x: pos.x + (Math.random() * (range * 2)) - range,
+        y: pos.y + (Math.random() * (range * 2)) - range,
+        z: pos.z
+    }
 }
 
-export function deleteLocalBlip(player, uniqueID) {
-    alt.emitClient(player, 'deleteLocalBlip', uniqueID);
+async function SetupCallback(player, eventName, args, callback) {
+    let promise = new Promise(async (resolve) => {
+        var results = await ClientsideCall(player, eventName, args).catch(
+            (err) =>  { return resolve(undefined); }
+        );
+        return resolve(results);
+    }); 
+
+    var finalResult = await promise;
+    callback(finalResult);
 }
 
-export function createMarker(player, type, pos, direction, rotation, scale, r, g, b, alpha, bobUpAndDown, faceCamera, isRotating, deleteOnEnter, range, uniqueID) {
-    alt.emitClient(player, 'createMarker', type, pos, direction, rotation, scale, r, g, b, alpha, bobUpAndDown, faceCamera, isRotating, deleteOnEnter, range, uniqueID);
-}
+async function ClientsideCall(player, eventName, args) {
+    return new Promise(async (resolve, reject) => {
+        if (player === undefined)
+            return reject(new Error('Player is undefined.'));
 
-export function deleteMarker(player, uniqueID) {
-    alt.emitClient(player, 'deleteMarker', uniqueID);
-}
+        if (eventName === undefined)
+            return reject(new Error('Event name is undefined.'));
 
-export function hideHUD(player, state) {
-    alt.emitClient(player, 'toggleHUD', state);
-}
+        alt.emitClient(player, eventName, args);
+        
+        const promise = new Promise((resolve) => {
+            alt.onClient(eventName, (player, results) => {
+                resolve(results);
+            });
+        })
 
-export function freezePlayer(player, state) {
-    alt.emitClient(player, 'freezePlayer', state);
-}
-
-export function interpolateCamera(player, pos1X, pos1Y, pos1Z, rot1, fov, pos2X, pos2Y, pos2Z, rot2, fov2, duration) {
-    alt.emitClient(player, 'interpolateCamera', pos1X, pos1Y, pos1Z, rot1, fov, pos2X, pos2Y, pos2Z, rot2, fov2, duration);
-}
-
-export function createCamera(player, pos, rot, fov) {
-    alt.emitClient(player, 'createCamera', pos, rot, fov);
-}
-
-export function destroyCamera(player) {
-    alt.emitClient(player, 'destroyCamera');
-}
-
-export function showNotification(player, imageName, headerMsg, detailsMsg, message) {
-    alt.emitClient(player, 'showNotification', imageName, headerMsg, detailsMsg, message);
-}
-
-export function distance(positionOne, positionTwo) {
-    return Math.pow(positionOne.x - positionTwo.x, 2) + Math.pow(positionOne.y - positionTwo.y, 2) + Math.pow(positionOne.z - positionTwo.z, 2);
+        var finalResult = await promise;
+        return resolve(finalResult);
+    });
 }
