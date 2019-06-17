2348293e60b702f7353bca8c7f15d57574ff40af
Trevor Wessel <stuykgaming@gmail.com>
2019-06-17 11:13:36 -0600

    Fixed Functions

--- server-extended/client.mjs
+++ server-extended/client.mjs
@@ -467,7 +467,7 @@ export function Screen2dToWorld3dPosition(absoluteX, absoluteY, flags, ignore, c
 // Get the Ground Location
 export function Get3DFrom2D(absoluteX, absoluteY, callback) {
     Screen2dToWorld3dPosition(absoluteX, absoluteY, 1, alt.getLocalPlayer().scriptID, (result) => {
-        callback(result);
+        callback(result[2]);
     });
 }
 
